82673e5695c11e8a2a98f6f9404b3cbb4cbf6263
gujjumukesh <you@example.com>
2025-08-23 21:40:51 +0530

    Fix: Install Rust and Cargo, and upgrade pip in build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# Install build tools, Python development headers, and MySQL client libraries
+# Install build tools, Python development headers, MySQL client libraries, Rust, and Cargo
 apt-get update && apt-get install -y build-essential python3-dev libmysqlclient-dev
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+export PATH="$HOME/.cargo/bin:$PATH"
 
-
-# Install Python dependencies
+# Update pip and install Python dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
